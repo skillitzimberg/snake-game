3aed3cf24383e2f894edac9a722dba408a122057
Scott Bergler <commandinghands@gmail.com>
2021-07-28 06:36:14 -0700

    As a PLAYER I want to see a snake's head on the board; refactors the game using a class

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-const canvas = document.getElementById("board");
-const ctx = canvas.getContext("2d");
+class SnakeGame {
+  constructor(canvas) {
+    this.ctx = canvas.getContext("2d");
+    this.center = {
+      width: canvas.width / 2,
+      height: canvas.height / 2,
+    };
+  }
 
-function drawBoard() {
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  drawBoard() {
+    this.ctx.fillStyle = "black";
+    this.ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+
+  drawSnake() {
+    this.ctx.fillStyle = "green";
+    this.ctx.fillRect(this.center.width, this.center.height - 20, 10, 10);
+  }
+
+  renderGame() {
+    this.drawBoard();
+    this.drawSnake();
+  }
 }
 
-drawBoard();
+const canvas = document.getElementById("board");
+const snakeGame = new SnakeGame(canvas);
+snakeGame.renderGame();
